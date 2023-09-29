c868f35b6304e8bd483f7481b6b32b02b1681bfc
Ariane <ariianeboorges@gmail.com>
2023-09-29 14:35:09 +0100

    feat: sidebar component - Add <Sidebar/> to <App/> - Install phosphor icons library

--- ignite/lessons/01-reactjs/src/App.jsx
+++ ignite/lessons/01-reactjs/src/App.jsx
@@ -1,8 +1,19 @@
 import {Header} from "./components/Header";
 import "./global.css";
+import styles from "./App.module.css";
+import {Sidebar} from "./components/Sidebar";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>feed</main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ ignite/lessons/01-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import styles from "./Sidebar.module.css";
+import {PencilLine} from "@phosphor-icons/react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1565660761890-0ecd9606690f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/arianeborges.png"
+        />
+        <strong>Ariane</strong>
+        <span>Frontend developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Edit your profile
+        </a>
+      </footer>
+    </aside>
+  );
+}

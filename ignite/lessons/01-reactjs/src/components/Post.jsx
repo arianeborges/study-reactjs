1eb17fc21d629b8043bde219297d52ab34261417
Ariane <ariianeboorges@gmail.com>
2023-09-29 15:28:53 +0100

    feat: post component - Create post header - Create post content

---
+++ ignite/lessons/01-reactjs/src/components/Post.jsx
@@ -0,0 +1,48 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header className={styles.header}>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/arianeborges.png"
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>Ariane</strong>
+            <span>Frontend developer</span>
+          </div>
+        </div>
+
+        <time
+          title="29th of September at 14:50pm"
+          dateTime="2023-09-29 14:50:00"
+        >
+          Published 1h ago
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Hey guys 👋 </p>
+        <p>
+          I just uploaded another project to my portfolio. It`&apos;`s a project
+          I did at NLW Return, a Rocketseat event. The name of the project is
+          DoctorCare 🚀
+        </p>
+        <p>
+          👉{"  "}
+          <a href="#">jane.design/doctorcare </a>
+        </p>
+        <p>
+          <a href="#">#newproject</a>
+          {"  "}
+          <a href="#">#nlw</a>
+          {"  "}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}

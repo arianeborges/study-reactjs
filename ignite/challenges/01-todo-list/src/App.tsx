ab3c9f8975b79de6ac7647a7679457f91240d67d
Ariane <ariianeboorges@gmail.com>
2023-10-04 16:02:12 +0100

    feat: create todo input

--- ignite/challenges/01-todo-list/src/App.tsx
+++ ignite/challenges/01-todo-list/src/App.tsx
@@ -1,11 +1,15 @@
-import "./App.module.css";
 import "./global.css";
 import {Header} from "./components/Header";
+import {ToDoInput} from "./components/ToDoInput";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <main className={styles.mainWrapper}>
+        <ToDoInput />
+      </main>
     </div>
   );
 }

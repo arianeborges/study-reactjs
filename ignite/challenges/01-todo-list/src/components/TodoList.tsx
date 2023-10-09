819ec2e06a4c87ed06fc301ba70fe8f001ead493
Ariane <ariianeboorges@gmail.com>
2023-10-09 17:30:06 +0100

    feat: handle create todo form

--- ignite/challenges/01-todo-list/src/components/TodoList.tsx
+++ ignite/challenges/01-todo-list/src/components/TodoList.tsx
@@ -1,37 +1,80 @@
-import {ClipboardText, PlusCircle} from "@phosphor-icons/react";
+import {ClipboardText, PlusCircle, Trash} from "@phosphor-icons/react";
 import styles from "./TodoList.module.css";
+import {ChangeEvent, FormEvent, useState} from "react";
 
 export function TaskList() {
+  const [todoValue, setTodoValue] = useState("");
+  const [arrayToDo, setArrayToDo] = useState([
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eius perferendis numquam minima corporis dolorum similique, vero quod odio inventore molestias voluptatem ut, error reprehenderit. Necessitatibus perspiciatis facilis molestiae autem",
+  ]);
+
+  function handleTodoChange(event: ChangeEvent<HTMLInputElement>) {
+    setTodoValue(event.target.value);
+  }
+
+  function handleCreateNewTodo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    setArrayToDo([...arrayToDo, todoValue]);
+
+    setTodoValue("");
+  }
+
   return (
     <div className={styles.mainWrapper}>
-      <div className={styles.inputWrapper}>
-        <input className={styles.input} placeholder="Add a new task" />
-        <button className={styles.button}>
+      <form
+        className={styles.inputWrapper}
+        onSubmit={(event) => handleCreateNewTodo(event)}
+      >
+        <input
+          name="todo"
+          className={styles.input}
+          placeholder="Add a new task"
+          value={todoValue}
+          onChange={(event) => handleTodoChange(event)}
+        />
+        <button className={styles.button} type="submit">
           Create
           <PlusCircle weight="bold" size={16} />
         </button>
-      </div>
+      </form>
 
       <div className={styles.mainContent}>
         <div className={styles.mainContentHeader}>
           <div className={styles.headerInfoLeft}>
             <p>Created tasks</p>
-            <span>0</span>
+            <span>{arrayToDo.length}</span>
           </div>
           <div className={styles.headerInfoRight}>
             <p>Completed</p>
-            <span>0</span>
+            <span>0 of {arrayToDo.length}</span>
           </div>
         </div>
 
         <div className={styles.taskList}>
-          <div className={styles.emptyTaskList}>
-            <ClipboardText size={64} />
-            <span>
-              <p>You still don't have any tasks registered</p>
-              <p>Create tasks and organize your to-dos</p>
-            </span>
-          </div>
+          {arrayToDo.length ? (
+            <div>
+              {arrayToDo.map((todo, index) => (
+                <div key={index} className={styles.itemContainer}>
+                  <div className={styles.itemContent}>
+                    <input type="checkbox" />
+                    <label>{todo}</label>
+                  </div>
+                  <div>
+                    <Trash size={24} />
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className={styles.emptyTaskList}>
+              <ClipboardText size={64} />
+              <span>
+                <p>You still don't have any tasks registered</p>
+                <p>Create tasks and organize your to-dos</p>
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </div>

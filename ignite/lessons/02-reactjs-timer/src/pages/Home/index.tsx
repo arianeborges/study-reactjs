3d50ac3c37aaf50d4d0f54024ee6fce281786eea
Ariane <ariianeboorges@gmail.com>
2023-10-12 11:22:24 +0100

    feat: finish input config and style

--- ignite/lessons/02-reactjs-timer/src/pages/Home/index.tsx
+++ ignite/lessons/02-reactjs-timer/src/pages/Home/index.tsx
@@ -19,13 +19,23 @@ export function Home() {
             placeholder="Give a name to your project"
             type="text"
             id="task"
+            list="task-suggestions"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Project 1" />
+            <option value="Project 2" />
+            <option value="Project 3" />
+          </datalist>
 
           <label htmlFor="minutesAmount">for</label>
           <MinutesAmountInput
             placeholder="00"
             type="number"
             id="minutesAmount"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutes.</span>
